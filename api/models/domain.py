9ea7fed5fd2fb21519eec869ea46a29c88d6c8e5
abraaovalentim <abraaotaku@gmail.com>
2025-03-27 22:06:40 -0300

    id domain auto

--- api/models/domain.py
+++ api/models/domain.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Domain(models.Model):
-    domain_id = models.BigIntegerField(primary_key=True)
+    domain_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     description = models.TextField()
